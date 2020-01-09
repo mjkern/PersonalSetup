bf28eff9630477c8d8b9daded0a9db33ad22026d
Matt <mjkern@andrew.cmu.edu>
2020-01-09 13:25:58 -0500

    beginning to setup bash shortcuts

--- wslSetup.sh
+++ wslSetup.sh
@@ -3,7 +3,13 @@
 # this is a setup script for all of my WSL preferences
 echo ~~~~~~~ Setting up WSL ~~~~~~~~
 
-### first, helper funtions ###
+### define a few constants ###
+ADDITION_FLAG="#~~~~~~~ added by personal setup script on $( date -I ) ~~~~~~~#"
+ADDITION_END_FLAG="#~~~~~~~ end of personal setup script additions ~~~~~~~#"
+#BASH_PROFILE='~/.bash_profile'
+BASH_PROFILE='testBashProfile.sh' # for testing
+
+### and helper funtions ###
 
 # given the status of a command, prints "complete" if status is 0 and prints
 # an error message otherwise
@@ -17,7 +23,7 @@ function reportAndContinue () {
   fi
 }
 
-### software instalation ###
+### software installation ###
 echo ~~~~~ Installing Software ~~~~~
 
 echo ~~ updating and upgrading packages
@@ -29,3 +35,17 @@ sudo apt install tree
 reportAndContinue $?
 
 echo ~~ Done Installing Software
+
+### bash shortcuts ###
+
+echo ~~~~~ Creating Bash Shortcuts ~~~~~
+
+echo >> $BASH_PROFILE
+echo $ADDITION_FLAG >> $BASH_PROFILE
+echo '### here are my custom shortcuts ###' >> $BASH_PROFILE
+echo ~~ cdd
+echo 'alias cdd="cd /mnt/c/Users/mjker/Documents"' >> $BASH_PROFILE
+echo '### this is the end of my custom shortcuts ###' >> $BASH_PROFILE
+echo $ADDITION_END_FLAG >> $BASH_PROFILE
+
+echo ~~ Done Creating Bash Shortcuts
