a69211fa619a35f2d276d828eee26300cf97229a
Matt <mjkern@andrew.cmu.edu>
2020-01-09 14:05:06 -0500

    first shot at vimrc

--- wslSetup.sh
+++ wslSetup.sh
@@ -8,6 +8,7 @@ ADDITION_FLAG="#~~~~~~~ added by personal setup script on $( date -I ) ~~~~~~~#"
 ADDITION_END_FLAG="#~~~~~~~ end of personal setup script additions ~~~~~~~#"
 #BASH_PROFILE='~/.bash_profile'
 BASH_PROFILE='testBashProfile.sh' # for testing
+VIMRC_URL='https://raw.githubusercontent.com/mjkern/PersonalSetup/master/.vimrc'
 
 ### and helper funtions ###
 
@@ -34,11 +35,16 @@ echo ~~ installing tree...
 sudo apt install tree
 reportAndContinue $?
 
+# TODO: vim and vimplug and vimpluggins
+# TODO: openssh
+
 echo ~~ Done Installing Software
 
 ### bash shortcuts ###
 
 echo ~~~~~ Creating Bash Shortcuts ~~~~~
+
+# possible point of improvement - only add the shortcuts that don't yet work
 
 echo >> $BASH_PROFILE
 echo $ADDITION_FLAG >> $BASH_PROFILE
@@ -49,3 +55,29 @@ echo '### this is the end of my custom shortcuts ###' >> $BASH_PROFILE
 echo $ADDITION_END_FLAG >> $BASH_PROFILE
 
 echo ~~ Done Creating Bash Shortcuts
+
+### configure vim ###
+echo ~~~~~ CONFIGURING VIM ~~~~~~~
+
+## download vimrc
+# possible point of improvement - simply backup the old .vimrc if it exists
+echo ~~ downloading .vimrc
+
+# be careful about overwriting the old .vimrc
+CONFIRMATION=y
+if [ -e ~/.vimrc ]; then
+  read -p '~/.vimrc already exists. would you like to overwrite it? (y/n)' CONFIRMATION
+fi
+
+if [ $CONFIRMATION = y ]; then
+  cd ~
+  if [ $? = 0 ]; then
+    curl -O $VIMRC_URL
+    reportAndContinue $?
+    cd -
+    if [ ! $0 = 0 ]; then
+      echo ~~ fatal error going back to directory
+      exit 1
+    fi
+  fi
+fi
