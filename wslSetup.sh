a6beed573364fe1df4ed2ed138d2825ea63e2f2a
Matt <mjkern@andrew.cmu.edu>
2020-01-09 12:45:13 -0500

    first shot at installing software

--- wslSetup.sh
+++ wslSetup.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo Setting up WSL
+# this is a setup script for all of my WSL preferences
+echo ~~~~~~~ Setting up WSL ~~~~~~~~
+
+### first, helper funtions ###
+
+#function reportAndContinue () {
+
+### software instalation ###
+echo ~~~~~ Installing Software ~~~~~
+
+echo updating and upgrading packages
+sudo apt update && sudo apt upgrade
+if [ $? -eq 0 ]; then
+  echo complete
+else
+  echo something went wrong... continuing anyway...
+fi
+
+echo installing tree...
+sudo apt install tree
+if [ $? -eq 0 ]; then
+  echo complete
+else
+  echo something went wrong... continuing anyway...
+fi
+
+echo Done Installing Software
